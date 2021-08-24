7e831ce1d59313cb397b307880424d17350582cd
Justus Croskery <justus@olmmcc.tk>
2021-08-23 20:21:16 -0400

    Add signup and login capabilities

--- root/signup/signup.js
+++ root/signup/signup.js
@@ -1,22 +1,21 @@
-let socket = new WebSocket("wss://" + location.host + "/ws/login");
+let socket = new WebSocket("wss://" + location.host + "/ws/signup");
 let messageSent = false;
+
+socket.onmessage = (e) => {
+    console.log(JSON.parse(e.data));
+    messageSent = false;
+};
 
 document.getElementById("submit").addEventListener("click", () => {
     if (!messageSent) {
         let email = document.getElementById("email").value;
         let password = document.getElementById("password").value;
+        let username = document.getElementById("username").value;
         socket.send(JSON.stringify({
             email,
-            password
+            password,
+            username
         }));
         messageSent = true;
     }
 });
-
-socket.onmessage = (e) => {
-    messageSent = false;
-    let date = new Date();
-    date.setTime(date.getTime() + (30 * 24 * 60 * 60 * 1000));
-    const expires = ";expires=" + date.toUTCString();
-    document.cookie="id=" + JSON.parse(e.data).id + expires;
-};
